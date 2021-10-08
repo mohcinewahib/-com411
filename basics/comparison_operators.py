1166ef920b0a3fbdd5c67465400d04d1109608f5
Mohcine Wahib <5wahim57@solent.ac.uk>
2021-10-08 15:45:33 +0100

    week2

---
+++ basics/comparison_operators.py
@@ -0,0 +1,8 @@
+x = int(input("enter first number; "))
+y = int(input("enter first number; "))
+if x < y :
+    print("the first number is smaller than the second number")
+elif x > y:
+    print("the first number is bigger than the second number")
+else:
+    print("the two numbers are equale")
---
+++ basics/main.py
@@ -0,0 +1,10 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+x = input("enter the type of the book; ")
+if x =="adventure":
+    print("I like adventure books!")
+
+print("Finished reading book.")
